9020c951109dc998b3f731f3d22e5961b1da84c5
Diremu Adebanjo <diremuadebanjo@gmail.com>
2024-01-14 20:52:20 +0100

    completed introduction

--- src/components/Introduction/Introduction.js
+++ src/components/Introduction/Introduction.js
@@ -3,7 +3,7 @@ import './Introduction.css'
 function Introduction () {
     return (
         <div>
-            <p>I'm <span>Diremu</span>. I will bring the grace to your ideas</p>
+            <p>I'm <span className="color-name">Diremu</span>. I will bring the grace to your ideas and breath to your projects.</p>
         </div>
     )
 }

f76471fff1f0b7ae8691b2cf26250e0ee88fff87
Diremu Adebanjo <diremuadebanjo@gmail.com>
2024-01-16 09:41:22 +0100

    started the main page

---
+++ src/About/About.js
@@ -0,0 +1,11 @@
+import './About.css';
+import Navbar from  '../components/Navbar/Navbar';
+
+function About () {
+    return (
+        <>
+           <Navbar />
+           
+        </>
+    )
+}
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,12 @@
 import './Navbar.css' 
+import Code from '../Images/code-solid.svg'
 
 const Navbar = () => {
     return(
         <nav>
             <ul>
                 <li>Diremu.</li>
-                <li className="selected"><a href="#">Home</a></li>
+                <li className="selected"><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#skills">Skills and Works</a></li>
                 <li><a href="#contact">Contact me</a></li>

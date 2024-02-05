c329b63c3a0191da0674db2e7724093d26e34e2f
Diremu Adebanjo <diremuadebanjo@gmail.com>
2024-02-05 13:49:57 +0100

    started next phase of cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,25 +1,41 @@
 import './Cards.css'
+import React, {useState} from 'react';
+
+function DivBuilder() {
+    return ( 
+    <>
+        <button type="button" id="popped-up">
+            <div className="hidden"></div>
+            <div className="content">
+                <h1>Education</h1>
+                <p>Hello there. i want to see the world sometime later.</p>
+            </div>
+            <div className="hidden"></div>
+        </button>
+    </>
+    )
+}
 
 export default function Cards() {
-
+    let [expand, setExpanded] = useState("");
     return (
         <>
         <div id="cards-container">
-            <a href="#first-card"  className="cards enlarged">
+            <button type="button" onClick={DivBuilder}  className={`cards ${expand === "first-card" ? "enlarged" : ""}`}>
                 <div id="first-card">
                 <h1>Education</h1>
             </div>
-            </a>
-            <a href="#second-card"  className="cards">
+            </button>
+            <button type="button" onClick={() => {setExpanded("second-card")}}  className={`cards ${expand === "second-card" ? "enlarged" : ""}`}>
                 <div id="second-card">
                 <h1>Education</h1>
             </div>
-            </a>
-            <a href="#third-card"  className="cards">
+            </button>
+            <button type="button" onClick={() => {setExpanded("third-card")}}  className={`cards ${expand === "third-card" ? "enlarged" : ""}`}>
                 <div id="third-card">
                 <h1>Education</h1>
             </div>
-            </a>
+            </button>
         </div>
         </>
     )

ca3c4b8186b076042f0deab4a6fd1922fc297dc8
Diremu Adebanjo <diremuadebanjo@gmail.com>
2024-02-06 13:48:54 +0100

    made it change size while hovering

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,29 +1,17 @@
 import './Cards.css'
 import React, {useState} from 'react';
 
-function DivBuilder() {
-    return ( 
-    <>
-        <button type="button" id="popped-up">
-            <div className="hidden"></div>
-            <div className="content">
-                <h1>Education</h1>
-                <p>Hello there. i want to see the world sometime later.</p>
-            </div>
-            <div className="hidden"></div>
-        </button>
-    </>
-    )
-}
-
 export default function Cards() {
     let [expand, setExpanded] = useState("");
     return (
         <>
         <div id="cards-container">
-            <button type="button" onClick={DivBuilder}  className={`cards ${expand === "first-card" ? "enlarged" : ""}`}>
+            <button type="button" onPointerOver={() => {setExpanded("first-card")}}  className={`cards ${expand === "first-card" ? "enlarged" : ""}`}>
                 <div id="first-card">
                 <h1>Education</h1>
+                <div className={`hidden ${expand === "first-card" ? "enlarged" : ""}`} >
+                    My education sbeeisjf sjhjsbhf sefk srh sheshbd haoweh.
+                </div>
             </div>
             </button>
             <button type="button" onClick={() => {setExpanded("second-card")}}  className={`cards ${expand === "second-card" ? "enlarged" : ""}`}>

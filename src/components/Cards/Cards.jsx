d06eb723db1f215ffa6709ff14c6635125e6fe4f
Diremu Adebanjo <diremuadebanjo@gmail.com>
2024-02-10 17:26:45 +0100

    last commit for 3 weeks

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -4,15 +4,13 @@ import React, {useState} from 'react';
 export default function Cards(props) {
     let [expand, setExpanded] = useState("");
     return (
-        <div id="cards-container">
             <div>
                 <div id={props.id} onMouseEnter={() => {setExpanded(props.id)}} onMouseLeave={() => {setExpanded("")}} className={`cards ${expand === props.id ? "enlarged" : ""} ${props.className}`}>
                 <h1 className="card-headings">{props.heading}</h1>
-                <div className={` ${expand === props.id ? "content" : "hidden"}`} >
-                    {props.content}
+                    <div className={` ${expand === props.id ? "content" : "hidden"}`} >
+                        {props.content}
+                    </div>
                 </div>
             </div>
-            </div>
-        </div>
     )
 }

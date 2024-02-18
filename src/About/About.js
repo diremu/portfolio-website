3821d129da5984f77d3117073bfcb6a89a599250
Diremu Adebanjo <diremuadebanjo@gmail.com>
2024-02-18 18:02:59 +0100

    typing animation

--- src/About/About.js
+++ src/About/About.js
@@ -2,27 +2,10 @@ import  './About.css';
 import Navbar from '../components/Navbar/Navbar';
 import Footer from '../components/Footer/Footer';
 import React from 'react';
-import { useInView } from 'react-intersection-observer';
 import Cards from '../components/Cards/Cards';
 
 const About = () => {
     document.title = "About | Diremu";
-    const [ref, inView] = useInView({triggerOnce: true,})
-    // document.addEventListener('DOMContentLoaded',function () {
-    //     const textElement = document.getElementById('learning-divh')
-    //     const textContent = textElement.trim('')
-    //     const letters = textContent.split('')
-
-    //     textElement.textContent = '';
-    //     letters.forEach((letter, index) => {
-    //         const span = document.createElement('span');
-    //         span.textContent = letter;
-    //         span.style.transitionDelay = `${index * 80}ms`; // Adjust the delay as needed
-    //         textElement.appendChild(span)
-    //     })
-    //     setTimeout(() => {
-    //         textElement.style.transform = 'translateY(0)';
-    //       }, 0)})
     return(
     <>
     <Navbar />
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,11 +1,30 @@
 import './Home.css'
 import Navbar from '../components/Navbar/Navbar';
+import { useInView } from 'react-intersection-observer'; 
+
 export default function Home() {
-    document.title = "Home | Diremu"
+    document.title = "Home | Diremu";
+    const [ref, inView] = useInView({triggerOnce: true,})
+    document.addEventListener('DOMContentLoaded',function () {
+        const textElement = document.getElementById('home-head')
+        const textContent = textElement.innerText
+        const letters = textContent.split('')
+
+        textElement.textContent = '';
+        letters.forEach((letter, index) => {
+            const span = document.createElement('span');
+            span.textContent = letter;
+            span.className = "";
+            span.style.transitionDelay = `${index * 120}ms`;
+            textElement.appendChild(span)
+        })
+        setTimeout(() => {
+            textElement.style.transform = 'translateY(0%)';
+          }, 2)})
     return (
         <>
             <Navbar />
-            <h2>Under Construction</h2>      
+            <h2 id="home-head" className={`${inView ? "visible" : "hidden"}`} ref={ref}>Under Construction</h2>      
         </>
     )
 }

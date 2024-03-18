0d5d9977e29b120199efad812d1240fd7dd0949a
Diremu Adebanjo <diremuadebanjo@gmail.com>
2024-03-18 19:54:34 +0100

    temp adjustments

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -4,6 +4,8 @@ const Projects = () => {
     return (
     <div id="projects">
         <h2>Under Construction</h2>
+        <p>some tight ass pussy is what kepps me going </p>
+        <p>all i need to kepp going is some pussy</p>
     </div>
     )
 }
